5ff4c5fac7d8b3d2798e63b47804efffc3bbd696
Armin Beiner <armin.beiner@hotmail.com>
2017-06-13 00:08:55 +0200

    Added Comments to db.js and index.js

--- db.js
+++ db.js
@@ -4,6 +4,10 @@
 'use strict'
 let Pool = require('pg').Pool
 
+/*Initialisierung der DB Verbindung mittels pg library.
+Definition als Pool für Verbindung von mehreren Clients gleichzeitig.
+ */
+
 let pool = new Pool({
     user: 'postgres',
     password: 'postgres',
@@ -14,13 +18,18 @@ let pool = new Pool({
 })
 
 export default class DB {
+    /*
+    Funktion für das Laden aller Fehler aus der Datenbank für die Initiale Ansicht aller erfassten Fehler
+     */
     getErrors(response){
 
           pool.on('error', function (e, client) {
             console.log(e)
         });
 
-
+        /*
+        SQL Query für die Abfrage der erfassten Fehler
+         */
         pool.query('SELECT ke.id, ke.title, stat.status, addby.name, cat.category FROM KnownErrors ke JOIN Status stat ON (ke.id_status = stat.id) JOIN Added_by addby ON (ke.id_added_by = addby.id) JOIN Category cat ON (ke.id_category = cat.id)', function (err, result) {
             //let test = JSON.parse.stringify(result.rows)
             //console.log(result.rows)
@@ -28,18 +37,28 @@ export default class DB {
         });
     }
 
+    /*
+    Funktion um alle Worklog Einträge für einen spezifischen Known Error aus der DB abzufragen.
+    Als Parameter wird die Known Error ID übergeben
+     */
     getWorklogs(id, response){
 
         pool.on('error', function (e, client){
             console.log(e)
         })
 
+        /*
+        SQL Abfrage um alle Worklog Einträge für einen Known Error auszulesen. Für die WHERE Bedingung wird die Known Error ID verwendet
+         */
        pool.query('SELECT wl.id, wl.title, wl.description, addby.name FROM Worklogs wl JOIN Added_by addby ON (wl.id_added_by = addby.id) WHERE id_known_error = ' + id+';', function (err, result) {
             response.json(result.rows);
         } )
 
     }
 
+    /*
+    Funktion um alle definierten Status aus der DB zu laden
+     */
     getStatus (response){
         pool.on('error', function (e, client){
             console.log(e)
@@ -51,6 +70,9 @@ export default class DB {
 
     }
 
+    /*
+    Funktion um alle erfassten Kategorien aus der DB zu laden
+     */
     getCategories (response ){
         pool.on('error', function (e, client){
             console.log(e)
@@ -61,6 +83,9 @@ export default class DB {
         })
     }
 
+    /*
+    Funktion um die Namen der Mitarbeiter aus der DB zu laden
+     */
     getNames (response) {
         pool.on('error', function (e, client){
             console.log(e)
@@ -71,22 +96,34 @@ export default class DB {
         })
     }
 
+    /*
+    Funktion um einen neuen Error hinzuzufügen.
+     */
     addErrors(title, id_status, id_added_by, id_category){
 
         pool.on('error', function (e, client){
             console.log(e)
         })
 
+        /*
+        SQL Query für INSERT. Die angegeben Variablen werden dem Query als Parameter übergeben
+         */
         pool.query('INSERT into KnownErrors(title, id_status, id_added_by, id_category) VALUES ($1, $2, $3, $4);',
         [title, id_status, id_added_by, id_category])
         //console.log('INSERT into KnownErrors(title, id_status, id_added_by, id_category) VALUES ($1, $2, $3, $4)')
     }
 
+    /*
+    Funktion um einen neuen Worklog bei einem spezifischen Known Error hinzuzufügen
+     */
     addWorklogs(title, description, id_added_by, id_category, id_known_error){
         pool.on('error', function (e, client) {
             console.log(e)
         })
 
+        /*
+        SQL Query für INSERT eines Worklogs. Die angegeben Variablen werden auch hier dem Query als Parameter übergeben
+         */
         pool.query('INSERT into Worklogs(title, description, id_added_by, id_category, id_known_error) VALUES ($1, $2, $3, $4, $5);',
         [title, description, id_added_by, id_category, id_known_error])
     }
--- index.js
+++ index.js
@@ -8,32 +8,54 @@ import bodyParser from 'body-parser'
 
 import DB from './db'
 
+/*
+Initialisierung der benötigen Klassen
+ */
 let app = express()
 app.use(bodyParser.json())
 app.use(cors())
 const data = new DB()
 const pg = require('pg');
 
+/*
+Startseite. Ruft die Funktion "getErrors" aus der DB aus und erhält alle erfassten Known Errors. Rückgabewert im JSON Format.
+ */
 app.get('/', function (req, res) {
     data.getErrors(res)
 })
 
+/*
+Ansicht für einen spezifischen Known Error. Ruft die "getWorklogs" Funktion aus der db.js auf und erhält alle erfassten Worklogs eines Known errors. Rückgabewert im JSON Format
+ */
 app.get('/ke', function(req, res){
     data.getWorklogs(1, res)
 })
 
+/*
+Lädt alle gültigen Statuses aus der Datenbank mit der Funktion "getStatus" aus der db.js. Wird für Dropdown Field im Client verwendet.
+ */
 app.get('/statuses', function(req, res) {
     data.getStatus(res)
 })
 
+/*
+Lädt alle gültigen Kategorien aus der Datenbank mit der Funktion "getCategories" aus der db.js. Wird für Dropdown Field im Client verwendet.
+ */
 app.get('/cat', function(req, res){
     data.getCategories(res)
 })
 
+/*
+Lädt die Namen aller berechtigten Mitarbeiter aus der Datenbank mit der Funktion "getNames" aus der db.js. Wird für Dropdown Field im Client verwendet.
+ */
 app.get('/names', function(req, res){
     data.getNames(res)
 })
 
+/*
+Fügt einen neuen Error zur Datenbank hinzu.
+ Die übermittelten Daten kommen im JSON Format daher. Die Parameter werden aus dem JSON ausgelesen und dem DB Query übergeben (Aufruf "addErrors" Funktion aus db.js)
+ */
 app.post('/add', function(req, res){
     let payload = req.body
     console.log('payload',payload)
@@ -50,7 +72,11 @@ app.post('/add', function(req, res){
     res.sendStatus(200)
 })
 
-app.post('add_wl', function(req, res){
+/*
+Fügt einem bestehenden Known Error einen neuen Worklog Eintrag hinzu.
+Aufbau Analog /add
+ */
+app.post('/add_wl', function(req, res){
     let payload = req.body
     let title = payload.title
     let description = payload.description
