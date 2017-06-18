49e6e7765c09b1976167f00226c56b2222836b72
arminbeiner <armin.beiner@hotmail.com>
2017-06-18 17:40:02 +0200

    Changed .get and .post functions in index.js Change to Router function from Express server

--- index.js
+++ index.js
@@ -12,43 +12,64 @@ import DB from './db'
 Initialisierung der benötigen Klassen
  */
 let app = express()
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cors())
 const data = new DB()
 const pg = require('pg');
 
+let router = express.Router()
+
 /*
 Startseite. Ruft die Funktion "getErrors" aus der DB aus und erhält alle erfassten Known Errors. Rückgabewert im JSON Format.
  */
-app.get('/', function (req, res) {
+
+router.use(function(req, res, next){
+    console.log('Log for console output hahaha')
+    next()
+})
+
+router.route('/')
+
+    .get(function (req, res){
     data.getErrors(res)
 })
 
 /*
 Ansicht für einen spezifischen Known Error. Ruft die "getWorklogs" Funktion aus der db.js auf und erhält alle erfassten Worklogs eines Known errors. Rückgabewert im JSON Format
  */
-app.get('/ke', function(req, res){
-    data.getWorklogs(1, res)
+
+router.route('/ke/:id')
+
+    .get(function(req, res){
+        //let test_id = req.param('ke_id')
+        let test_id = req.params.id
+        console.log('*****TEST_ID*******' + test_id)
+    //console.log('*****ID****' + reg.params.id)
+    data.getWorklogs(test_id, res)
 })
 
 /*
 Lädt alle gültigen Statuses aus der Datenbank mit der Funktion "getStatus" aus der db.js. Wird für Dropdown Field im Client verwendet.
  */
-app.get('/statuses', function(req, res) {
+router.route('/statuses')
+    .get( function(req, res){
     data.getStatus(res)
 })
 
 /*
 Lädt alle gültigen Kategorien aus der Datenbank mit der Funktion "getCategories" aus der db.js. Wird für Dropdown Field im Client verwendet.
  */
-app.get('/cat', function(req, res){
+router.route('/cat')
+    .get (function(req, res){
     data.getCategories(res)
 })
 
 /*
 Lädt die Namen aller berechtigten Mitarbeiter aus der Datenbank mit der Funktion "getNames" aus der db.js. Wird für Dropdown Field im Client verwendet.
  */
-app.get('/names', function(req, res){
+router.route('/names')
+    .get(function(req, res){
     data.getNames(res)
 })
 
@@ -56,7 +77,8 @@ app.get('/names', function(req, res){
 Fügt einen neuen Error zur Datenbank hinzu.
  Die übermittelten Daten kommen im JSON Format daher. Die Parameter werden aus dem JSON ausgelesen und dem DB Query übergeben (Aufruf "addErrors" Funktion aus db.js)
  */
-app.post('/add', function(req, res){
+router.route('/add')
+    .post(function(req, res){
     let payload = req.body
     console.log('payload',payload)
     let title = payload.title
@@ -76,7 +98,8 @@ app.post('/add', function(req, res){
 Fügt einem bestehenden Known Error einen neuen Worklog Eintrag hinzu.
 Aufbau Analog /add
  */
-app.post('/add_wl', function(req, res){
+router.route('/add_wl')
+    .post(function(req, res){
     let payload = req.body
     let title = payload.title
     let description = payload.description
@@ -87,6 +110,8 @@ app.post('/add_wl', function(req, res){
     data.addWorklogs(title, description, name, category, id_known_error)
     res.sendStatus(200)
 })
+
+app.use('/', router)
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
